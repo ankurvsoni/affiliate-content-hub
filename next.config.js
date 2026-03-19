590f7b25790566c3a13cc26a7b66b59eda09c4de
ankurvsoni <ankurvsoni@users.noreply.github.com>
2026-03-19 09:51:55 -0700

    Rename tools surface to reviews and redirect /tools

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: { unoptimized: true },
+  async redirects() {
+    return [
+      {
+        source: '/tools',
+        destination: '/reviews',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
